74f2684d90ae0b453e6293264f13878b9dcc7b8e
Hajile-Haji <28717423+Hajile-Haji@users.noreply.github.com>
2018-03-07 23:09:37 -0600

    Update and rename MangaDex MAL & MU Search Tool.js to mal-and-mu-search-tool.user.js

--- mal-and-mu-search-tool.user.js
+++ mal-and-mu-search-tool.user.js
@@ -1,9 +1,11 @@
 // ==UserScript==
-// @name      MangaDex MAL & MU Search Tool
-// @author    Hajile-Haji
-// @namespace Violentmonkey Scripts
-// @match     https://mangadex.com/manga/*
-// @grant     none
+// @name         MangaDex MAL & MU Search Tool
+// @author       Hajile-Haji
+// @homepage     https://github.com/Hajile-Haji/mangadex-scripts
+// @updateURL    https://raw.githubusercontent.com/Hajile-Haji/mangadex-scripts/master/mal-and-mu-search-tool.user.js
+// @downloadURL  https://raw.githubusercontent.com/Hajile-Haji/mangadex-scripts/master/mal-and-mu-search-tool.user.js
+// @match        https://mangadex.com/manga/*
+// @grant        none
 // ==/UserScript==
 
 (() => {
